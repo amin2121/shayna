a0330ca59e049c9e5a8566fc49950e6e0030565e
amin2121 <35768970+amin2121@users.noreply.github.com>
2020-12-02 13:18:07 +0700

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<Header/>
+		<HeroCarousel/>
+		<ProductItem/>
+		<InstagramPhoto/>
+		<PartnerLogo/>
+		<Footer/>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header';
+import HeroCarousel from '@/components/HeroCarousel';
+import ProductItem from '@/components/ProductItem';
+import InstagramPhoto from '@/components/InstagramPhoto';
+import PartnerLogo from '@/components/PartnerLogo';
+import Footer from '@/components/Footer';
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    Header,
+    HeroCarousel,
+    ProductItem,
+    InstagramPhoto,
+    PartnerLogo,
+    Footer
+  }
+}
+</script>
